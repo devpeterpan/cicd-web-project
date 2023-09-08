40e1587ad362531658b8803cc0460cb89251f145
LEESEOKJUN <happynordic@gmail.com>
2023-09-08 20:55:46 +0900

    trigger test

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -15,7 +15,7 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        logger.debug("Welcome index ~");
+        logger.debug("Welcome ~");
 
         model.addAttribute("msg", getMessage());
         model.addAttribute("today", new Date());
